fa0c099e1fb316e4238956c1d98f50cabd8f9952
chronicSurfer <kyle.umbarger@gmail.com>
2019-01-26 23:46:59 -0800

    Deletions and modifications

--- website/src/index.js
+++ website/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#index.html">Huntington State Beach Lifeguard Association</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse pull-right" id="navbarNavDropdown">
+                  <ul className="navbar-nav">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="#">Meet Your Board <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Sponsors</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Log In</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Nav/>, document.querySelector('#root'));
